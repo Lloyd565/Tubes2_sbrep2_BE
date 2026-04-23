f4793c674749664a3ef9d03b3e224d42d0656be0
Lloyd565 <samsr472@gmail.com>
2026-04-24 06:05:11 +0700

    fix : manual parse

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -6,6 +6,13 @@ import (
 	"golang.org/x/net/html"
 )
 
+var selfClosing = map[string]bool{
+	"area": true, "base": true, "br": true, "col": true,
+	"embed": true, "hr": true, "img": true, "input": true,
+	"link": true, "meta": true, "param": true, "source": true,
+	"track": true, "wbr": true,
+}
+
 type Node struct {
 	Tag        string
 	ID         string
@@ -17,101 +24,138 @@ type Node struct {
 }
 
 func Parse(htmlStr string) (*Node, error) {
-	doc, err := html.Parse(strings.NewReader(htmlStr))
-	if err != nil {
-		return nil, err
-	}
+	tokenizer := html.NewTokenizer(strings.NewReader(htmlStr))
 
-	var root *html.Node
-	var findHTML func(*html.Node)
-	findHTML = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "html" {
-			root = n
-			return
+	virtualRoot := &Node{Tag: "#document", Depth: -1, Attributes: make(map[string]string)}
+	stack := []*Node{virtualRoot}
+
+	for {
+		tt := tokenizer.Next()
+
+		if tt == html.ErrorToken {
+			break
 		}
-		for child := n.FirstChild; child != nil; child = child.NextSibling {
-			findHTML(child)
+
+		switch tt {
+		case html.StartTagToken, html.SelfClosingTagToken:
+			rawName, hasAttr := tokenizer.TagName()
+			tagName := string(rawName)
+
+			parent := stack[len(stack)-1]
+			node := &Node{
+				Tag:        tagName,
+				Parent:     parent,
+				Depth:      parent.Depth + 1,
+				Attributes: make(map[string]string),
+			}
+
+			for hasAttr {
+				var k, v []byte
+				k, v, hasAttr = tokenizer.TagAttr()
+				key, val := string(k), string(v)
+				node.Attributes[key] = val
+				switch key {
+				case "id":
+					node.ID = val
+				case "class":
+					node.Classes = strings.Fields(val)
+				}
+			}
+
+			parent.Children = append(parent.Children, node)
+
+			isSelfClose := tt == html.SelfClosingTagToken || selfClosing[tagName]
+			if !isSelfClose {
+				stack = append(stack, node)
+			}
+
+		case html.EndTagToken:
+			rawName, _ := tokenizer.TagName()
+			tagName := string(rawName)
+
+			for len(stack) > 1 {
+				top := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				if top.Tag == tagName {
+					break
+				}
+			}
 		}
 	}
-	findHTML(doc)
 
-	return buildNode(root, nil, 0), nil
-}
-
-func buildNode(n *html.Node, parent *Node, depth int) *Node {
-	if n == nil {
-		return nil
-	}
-
-	node := &Node{
-		Tag:        n.Data,
-		Parent:     parent,
-		Depth:      depth,
-		Attributes: make(map[string]string),
-	}
-
-	for _, attr := range n.Attr {
-		node.Attributes[attr.Key] = attr.Val
-		if attr.Key == "id" {
-			node.ID = attr.Val
-		}
-		if attr.Key == "class" {
-			node.Classes = strings.Fields(attr.Val)
+	for _, child := range virtualRoot.Children {
+		if child.Tag == "html" {
+			child.Parent = nil
+			return child, nil
 		}
 	}
-
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		if c.Type == html.ElementNode {
-			child := buildNode(c, node, depth+1)
-			node.Children = append(node.Children, child)
-		}
+	if len(virtualRoot.Children) > 0 {
+		root := virtualRoot.Children[0]
+		root.Parent = nil
+		return root, nil
 	}
 
-	return node
+	return virtualRoot, nil
 }
 
 func CombinatorParser(selector string) []string {
 	var res []string
-	idx := 0
-	for i, str := range selector {
-		if str == ' ' {
-			if selector[i-1] == ',' {
-				res = append(res, selector[idx:i-2])
-				res = append(res, selector[i-1:i-1])
-			} else {
-				res = append(res, selector[idx:i-1])
-			}
-			idx = i + 1
+	var cur strings.Builder
+
+	flushCur := func() {
+		tok := strings.TrimSpace(cur.String())
+		if tok != "" {
+			res = append(res, tok)
 		}
+		cur.Reset()
 	}
+
+	i := 0
+	for i < len(selector) {
+		ch := selector[i]
+		switch {
+		case ch == ' ':
+			flushCur()
+			for i+1 < len(selector) && selector[i+1] == ' ' {
+				i++
+			}
+
+		case ch == '>' || ch == '+' || ch == '~':
+			flushCur()
+			res = append(res, string(ch))
+
+		case ch == ',':
+			flushCur()
+			res = append(res, ",")
+
+		default:
+			cur.WriteByte(ch)
+		}
+		i++
+	}
+	flushCur()
 	return res
 }
 
 func SelectorParser(selector string) []string {
 	var res []string
 	idx := 0
-	for i, str := range selector {
-		if (str == '.' || str == '[' || str == '#') && i != 0 {
-			res = append(res, selector[idx:i-1])
+	for i, ch := range selector {
+		if (ch == '.' || ch == '[' || ch == '#') && i != 0 {
+			res = append(res, selector[idx:i])
 			idx = i
 		}
 	}
+	res = append(res, selector[idx:])
 	return res
 }
 
 func AttrParser(attrs string) (string, string) {
-	attrs = attrs[1 : len(attrs)-2]
-	idx := 0
-	var attr string
-	for i, str := range attrs {
-		if str == '=' {
-			attr = attrs[idx : i-1]
-			idx = i + 1
-			break
-		}
-		if i == len(attrs)-1 {
-			return attrs, ""
+	attrs = attrs[1 : len(attrs)-1]
+	for i, ch := range attrs {
+		if ch == '=' {
+			return attrs[:i], attrs[i+1:]
 		}
 	}
-	return attr, attrs[idx:]
+	return attrs, ""
 }
